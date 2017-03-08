b8b2708f4cb7ebdfaa9db30fdc1f4760d1643cc9
RDP <rdupaw@gmail.com>
2017-03-07 22:05:36 -0600

    Delete individual scope props and put all into one prop array 'apps'

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,29 +1,40 @@
 app.controller('MainController', ['$scope', function($scope) {
-  $scope.move = {
-    icon: 'img/move.jpg',
-    title: 'MOVE',
-    developer: 'MOVE, Inc.',
-    price: 0.99
-  };
-
-  $scope.shutterbugg = {
-    icon: 'img/shutterbugg.jpg',
-    title: 'Shutterbugg',
-    developer: 'Chico Dusty',
-    price: 2.99
-  };
-
-  $scope.gameboard = {
-    icon: 'img/gameboard.jpg',
-    title: 'Gameboard',
-    developer: 'Armando P.',
-    price: 1.99
-  };
-
-  $scope.forecast = {
-    icon: 'img/forecast.jpg',
-    title: 'Forecast',
-    developer: 'Forecast',
-    price: 1.99
-  }
+  $scope.apps = [
+    {
+      icon: 'img/move.jpg',
+      title: 'MOVE',
+      developer: 'MOVE, Inc.',
+      price: 0.99
+    },
+    {
+      icon: 'img/shutterbugg.jpg',
+      title: 'Shutterbugg',
+      developer: 'Chico Dusty',
+      price: 2.99
+    },
+    {
+      icon: 'img/gameboard.jpg',
+      title: 'Gameboard',
+      developer: 'Armando P.',
+      price: 1.99
+    },
+    {
+      icon: 'img/forecast.jpg',
+      title: 'Forecast',
+      developer: 'Forecast',
+      price: 1.99
+    },
+    {
+      icon: 'img/uber.jpg',
+      title: 'Uber',
+      developer: 'Uber Technologies, Inc.',
+      price: 0
+    },
+    {
+      icon: 'img/facebook.jpg',
+      title: 'Facebook',
+      developer: "Facebook, Inc.",
+      price: 0
+    }
+  ];
 }]);
